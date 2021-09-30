e0a52382798d013bd2b853e7b4a1806cc9309710
zpc-666 <55796038+zpc-666@users.noreply.github.com>
2021-09-30 22:44:14 +0800

    Delete DNSD directory

--- DNSD/run.sh
+++
@@ -1,2 +0,0 @@
-#单机单卡下进行训练
-python -m paddle.distributed.launch --gpus "0" main.py --n_gpu 1 --name Cifar100-ViT_B16 --gradient_accumulation_steps 32 --fp16
--- DNSD/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .scheduler import WarmupLinearSchedule, WarmupCosineSchedule
-from .data_utils import get_loader
-from .utils import *
